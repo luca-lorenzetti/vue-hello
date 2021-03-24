09fff2881486db795a5d7859ad07ffb1f2cf28bd
luca <luca.lorenzetti0@gmail.com>
2021-03-24 14:07:32 +0100

    Creazione progetto

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(function(){
+
+let giocatore = {
+    nome: "Ibrahimovich Zlatan",
+    squadra: "Milan",
+    ruolo : "A",
+    partite: 15,
+    gol: 15,
+    ammonizioni: 2,
+    espulsioni: 0,
+    assistFermo: [1,0],
+    rigoriSegnati: [3,6],
+    mediaVoto: 6.54,
+    mediaFantaVoto: 9.11,
+    quotazione: 35.0,
+    quotazioneAttuale: 33.0 
+}
+
+});
