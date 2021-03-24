b9f114dfaf1e6c12ec3df1a1a2c7f512b700a8b0
luca <luca.lorenzetti0@gmail.com>
2021-03-24 14:32:00 +0100

    Inserito vueHello

--- js/script.js
+++ js/script.js
@@ -1,19 +1,7 @@
-$(function(){
-
-let giocatore = {
-    nome: "Ibrahimovich Zlatan",
-    squadra: "Milan",
-    ruolo : "A",
-    partite: 15,
-    gol: 15,
-    ammonizioni: 2,
-    espulsioni: 0,
-    assistFermo: [1,0],
-    rigoriSegnati: [3,6],
-    mediaVoto: 6.54,
-    mediaFantaVoto: 9.11,
-    quotazione: 35.0,
-    quotazioneAttuale: 33.0 
-}
-
-});
+var app = new Vue({
+    el: '#app',
+    data: {
+     title: 'Ciao sono fatto in VUEJS',
+     image: 'https://i.picsum.photos/id/1021/2048/1206.jpg?hmac=fqT2NWHx783Pily1V_39ug_GFH1A4GlbmOMu8NWB3Ts'
+    }
+  })
